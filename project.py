a128514e9a47a84585a5598292c5a2446e0ffd8c
Kashish Jaiswal <kashishjais23@gmail.com>
2021-12-12 12:33:17 +0530

    Day2

--- project.py
+++ project.py
@@ -1,7 +1,9 @@
 import os
 import telebot
-API_KEY=os.getenv('API_KEY')
-bot=telebot.TeleBot(API_KEY)
+import time
+
+API_KEY='5089223454:AAH6ADkjcBQbRWkhJErpixgSkEj1weh8WI4'
+bot=telebot.TeleBot(token=API_KEY)
 
 @bot.message_handler(commands=['Greet'])
 def greet(message):
@@ -10,4 +12,18 @@ def greet(message):
 @bot.message_handler(commands=['Hello'])
 def hello(message):
     bot.send_message(message.chat.id,'Hello! Quirkyy this side')
+
+@bot.message_handler(commands=['Help'])
+def help(message):
+    bot.reply_to(message,'to use this bot,send your username')    
+
+def find_at(msg):
+    for text in msg:
+        if '@' in text:
+            return text
+@bot.message_handler(func=lambda msg:msg.text is not None and '@'  in msg.text)
+def at_answer(message):
+    texts=message.text.split()
+    at_text=find_at(texts)
+    bot.reply_to(message,'https://instagram.com/{}'.format(at_text[1:]))
 bot.polling()
