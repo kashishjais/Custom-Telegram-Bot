84f59683ae7531536e87a897716d51c06fc37cf0
Kashish Jaiswal <kashishjais23@gmail.com>
2021-12-14 13:45:12 +0530

    .

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ import telegram.ext
 from datetime import datetime
 from telebot import types, TeleBot
 from telebot.types import Update
+from newsapi.newsapi_client import NewsApiClient
 
 def main():
     updater=Update(keys.API_KEY)
@@ -52,3 +53,26 @@ def at_answer(message):
 bot.polling()
 
 
+# Init
+newsapi = NewsApiClient(api_key='18b3064537594d28bccda78d7c20c1a7')
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          sources='bbc-news,the-verge',
+                                          category='business',
+                                          language='en',
+                                          country='in')
+
+# /v2/everything
+all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.in,techcrunch.com',
+                                      from_param='2017-12-01',
+                                      to='2017-12-12',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+
+# /v2/top-headlines/sources
+sources = newsapi.get_sources()
+print(top_headlines)
